9f509a802a050bec3a2f50af52d1bf071885c96f
saidone <saidone@saidone.org>
2022-03-14 07:41:41 +0100

    a couple of tests

--- test/holy_bibl/core_test.clj
+++ test/holy_bibl/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [holy-bibl.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest Luke-6-21
+  (= (Luke "6:21") "Blessed are ye that hunger now: for ye shall be filled. Blessed are ye that weep now: for ye shall laugh."))
+
+(deftest Luke-12-28
+  (= (Luke "12:28") "And if he shall come in the second watch, and if in the third, and find them so, blessed are those servants."))
