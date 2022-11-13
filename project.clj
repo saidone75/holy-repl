00a08e1c0bc0169edcbb1a638bd262744e6be18b
saidone <saidone@saidone.org>
2022-11-13 15:41:09 +0100

    rename project

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject holy-bibl "0.1.0-SNAPSHOT"
+(defproject holy-repl "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.xml "0.0.8"]]
-  :main ^:skip-aot holy-bibl.core
+  :main ^:skip-aot holy-repl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
