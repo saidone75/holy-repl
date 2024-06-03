6408343707e84bb37bd470db0eaf03ddb0918b89
saidone <saidone@saidone.org>
2024-06-03 22:36:07 +0200

    secure random

--- src/holy_repl/core.clj
+++ src/holy_repl/core.clj
@@ -1,4 +1,5 @@
 (ns holy-repl.core
+  (:import [java.security SecureRandom])
   (:gen-class))
 
 (require '[clojure.data.xml :as xml]
@@ -94,23 +95,56 @@
 
 (defn random-verse
   "Returns a map entry representing a random verse from a random book.
-  The key is the name of the book followed by chapter and verse numbers.
+  The key is the name of the book followed by chapter and verse numbers.\\
+  For divination purposes at higher accuracy see also [[secure-random-verse]].
   ```clojure
   (random-verse)
-  => [\"John 2:6\" (\"Now there were six waterpots of stone set there after the Jews' manner of purifying, containing two or three firkins apiece.\")]
+  => [\"John 2:6\" (\"Now there were six waterpots of stone set there after the Jews' manner of purifying, containing
+  two or three firkins apiece.\")]
   ```"
   []
   (let [book-name (first (shuffle (books)))
-        book (ns-resolve *ns* (symbol book-name))
+        book (resolve (symbol book-name))
         book-map (zipmap (range 1 (inc (count (book)))) (map #(inc (rand-int (count (:content %)))) (book)))
         chapter (rand-nth (keys book-map))]
     (first {(str book-name " " chapter ":" (get book-map chapter)) (book (str chapter ":" (get book-map chapter)))})))
 
+(defn secure-random-verse
+  "Returns a map entry representing a random verse from a random book.
+  The key is the name of the book followed by chapter and verse numbers.\\
+  Unlike [[random-verse]] this uses a FIPS 140-2 compliant RNG to determine all collections indices.
+  ```clojure
+  (random-verse)
+  => [\"Deut 9:12\"\n (\"And Jehovah said unto me, Arise, get thee down quickly from hence; for thy people that thou
+  hast brought forth out of Egypt have corrupted themselves; they are quickly turned aside out of the way which I
+  commanded them; they have made them a molten image.\")]
+  ```"
+  []
+  (let [book-name (nth (books) (.nextInt (SecureRandom.) (count (books))))
+        book (resolve (symbol book-name))
+        book-map (zipmap (range 1 (inc (count (book)))) (map #(inc (.nextInt (SecureRandom.) (count (:content %)))) (book)))
+        chapter (nth (keys book-map) (.nextInt (SecureRandom.) (count book-map)))]
+    (first {(str book-name " " chapter ":" (get book-map chapter)) (book (str chapter ":" (get book-map chapter)))})))
+
 (defn random-verse-str
-  "Returns a random verse as a string.
+  "Returns a random verse as a string.\\
+  For divination purposes at higher accuracy see also [[secure-random-verse-str]].
   ```clojure
   (random-verse-str)
-  => \"O Jehovah, to thee do I cry; for the fire hath devoured the pastures of the wilderness, and the flame hath burned all the trees of the field.\"
+  => \"O Jehovah, to thee do I cry; for the fire hath devoured the pastures of the wilderness, and the flame hath burned
+  all the trees of the field.\"
   ```"
   []
   ((comp first val) (random-verse)))
+
+(defn secure-random-verse-str
+  "Returns a random verse as a string.\\
+  Unlike [[random-verse-str]] this uses a FIPS 140-2 compliant RNG to determine all collections indices.
+  ```clojure
+  (random-verse-str)
+  => \"And Absalom answered Joab, Behold, I sent unto thee, saying, Come hither, that I may send thee to the king, to
+  say, Wherefore am I come from Geshur? it were better for me to be there still. Now therefore let me see the king's
+  face; and if there be iniquity in me, let him kill me.\"
+  ```"
+  []
+  ((comp first val) (secure-random-verse)))
